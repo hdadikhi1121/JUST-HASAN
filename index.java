43da3cf799c0032863b4504372f478adbd492cf0
hdadikhi1121 <110852728+hdadikhi1121@users.noreply.github.com>
2022-09-18 21:02:02 +0300

    edit

--- index.java
+++ index.java
@@ -2,15 +2,11 @@
 class Index {
     public static void main(String[] args) {
         Person p1 = new Person(1, "000", "sunbul@gmail.com", "112233344");
-        Address a1 = new Address(1,"first", "first", "first", "first", );
-        Student s1 = new Student(id:1, studentNumber:"45", averageMar   k:"55", personId:"1");
-
+        Address a1 = new Address(1,"first", "first", "first", "first","" );
         Person.persons.add(p1);
         Person.persons.remove(p1);
-        System.out.println(p1);
         Address.addresss.add(a1);
         Address.addresss.remove(a1);
-        Student.students.add(s1);
-
+        
     }
 }
