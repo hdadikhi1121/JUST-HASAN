038130b49f160ea34b5c3d7a4955e4c89945e242
hdadikhi1121 <110852728+hdadikhi1121@users.noreply.github.com>
2022-09-19 09:27:12 +0300

    new edet

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
+import javax.sound.sampled.SourceDataLine;
 
-class Index {
+class Main {
     public static void main(String[] args) {
         Person p1 = new Person(1, "000", "sunbul@gmail.com", "112233344");
         Address a1 = new Address(1,"first", "first", "first", "first","" );
+        Student s1 = new Student(2, "45", "55", "2");
         Person.persons.add(p1);
         Person.persons.remove(p1);
         Address.addresss.add(a1);
         Address.addresss.remove(a1);
+        Student.students.add(s1);
+        System.out.println(p1.toString());
+        System.out.println(a1.toString());
+        System.out.println(s1.toString());
         
     }
 }
